0b782c3160b2dc8538904b29d7bada28469a6f46
Pavel Dusatko <pavel.dusatko@gmail.com>
2023-03-13 14:47:27 -0700

    Add constants

---
+++ OneRepMax/Constants.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Constants {
+    // An array of weight plates sorted from the heaviest to the lightest
+    static let weightIncrementsDescending = [45.0, 35.0, 25.0, 10.0, 5.0, 2.5]
+}

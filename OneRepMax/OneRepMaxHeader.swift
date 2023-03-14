a29d9661f19f9dfbdafef1f0958a611f61eabca4
Pavel Dusatko <pavel.dusatko@gmail.com>
2023-03-14 16:30:07 -0700

    Prepare header view

---
+++ OneRepMax/OneRepMaxHeader.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct OneRepMaxHeader: View {
+    private let exercise: Exercise
+    private let foregroundColor: Color
+    private let dateFormatter: DateFormatter
+    
+    init(
+        dateFormatter: DateFormatter,
+        exercise: Exercise,
+        foregroundColor: Color
+    ) {
+        self.dateFormatter = dateFormatter
+        self.exercise = exercise
+        self.foregroundColor = foregroundColor
+    }
+    
+    var body: some View {
+        HStack {
+            Text(self.dateFormatter.string(from: self.exercise.date))
+                .font(.title2)
+                .foregroundColor(self.foregroundColor)
+            Spacer()
+            Text(String(format: "%.1f", self.exercise.oneRepMax))
+                .font(.title)
+                .foregroundColor(self.foregroundColor)
+        }
+    }
+}
